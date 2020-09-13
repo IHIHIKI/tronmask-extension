77cccaeee45d3fc95c974016edb411acffa38539
Kevin Holder <kevholderx@gmail.com>
2020-09-13 15:17:26 -0400

    injected tronweb: lazily request account access and reload page

--- app/scripts/lib/setupTronweb.js
+++ app/scripts/lib/setupTronweb.js
@@ -1,11 +1,48 @@
 import { createTronwebFromProvider } from '@opentron/tronweb-from-provider';
+import { ethAddress } from '@opentron/tron-eth-conversions';
+import EventEmitter from 'events';
 
 // TODO....
 
 export default function setupTronweb (tronmaskProvider) {
   // export web3 as a global, checking for usage
 
-  const tronweb = createTronwebFromProvider(tronmaskProvider)
+  const tronwebTarget = createTronwebFromProvider(tronmaskProvider)
+
+  const propAccessEmitter = new EventEmitter()
+
+  // Proxy
+  const tronweb = new Proxy(tronwebTarget, {
+    get: (...args) => {
+      const [target, prop, receiver] = args
+      propAccessEmitter.emit(prop, { target, prop, receiver })
+      return Reflect.get(...args)
+    },
+  })
+
+  // Detect if tronWeb.defaultAddress is accessed... if yes, requestAccounts,
+  // set defaultAddress and refresh page?
+  async function assignDefaultAddress () {
+    try {
+      const accounts = await tronmaskProvider.request({ method: 'eth_accounts' })
+      const addressHex = accounts[0]
+      if (!addressHex) {
+        console.log('Tronmask not authorized yet. Monitoring tronweb.defaultAddress access.')
+        // "lazily" authorize site if tronweb.defaultAddress is accessed
+        propAccessEmitter.once('defaultAddress', async () => {
+          await tronmaskProvider.request({ method: 'eth_requestAccounts' })
+          reloadPage()
+        })
+        return
+      }
+      const defaultAddress = ethAddress.toTron(addressHex)
+      tronweb.setAddress(defaultAddress)
+    } catch (err) {
+      console.error('error getting defaultAddress: ', err)
+    }
+  }
+
+  assignDefaultAddress()
 
   // TODO: detect if window.tronWeb already set?
   Object.defineProperty(global, 'tronWeb', {
@@ -29,8 +66,6 @@ export default function setupTronweb (tronmaskProvider) {
 
 // reload the page
 
-/*
-function triggerReset () {
+function reloadPage () {
   global.location.reload()
 }
-*/
